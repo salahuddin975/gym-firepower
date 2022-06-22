86b69411d5517b54ff3a167cbf27e55501525a0c
salahuddin975 <salahuddin975@gmail.com>
2022-06-21 22:42:00 -0500

    update

--- gym_firepower/envs/firepower_env.py
+++ gym_firepower/envs/firepower_env.py
@@ -108,14 +108,8 @@ class FirePowerEnv(gym.Env):
 
     def _get_reward(self):
         if self.power_sys_model.has_converged:
-            # load_loss = -1 * (self.ppc["baseMVA"] * self.power_sys_model.get_load_loss())
             load_loss = -1 * self.power_sys_model.get_load_loss()
-            protection_penalty = self.power_sys_model.get_protection_operation_count() * self.pa_penalty
-            active_line_removal = -1 * self.power_sys_model.get_active_line_removal_penalty() * 25
-
             return load_loss, load_loss
-            # return load_loss + active_line_removal, load_loss
-            # return load_loss + protection_penalty + active_line_removal, load_loss
         else:
             return (self.nc_penalty, 0)
 
